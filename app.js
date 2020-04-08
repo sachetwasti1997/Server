349b29037466d8e05cbb909f84b882f3a7dda848
sachet1997 <sachetwasti61@gmail.com>
2020-04-08 14:16:24 +0530

    changed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const exp = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./models/User');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
 require('./services/passport');
 
@@ -11,6 +12,17 @@ mongoose.connect(keys.mongoURI,
     .catch(err => console.log(err));
 
 const app = exp();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey] 
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 8000
